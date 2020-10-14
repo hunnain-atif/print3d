a59a98ac8f5772647d10c7655919291c80ce6f29
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-10-13 23:09:32 -0400

    created file upload component

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import Dropzone from 'react-dropzone'; 
+import {Icon} from 'antd'; 
 function FileUpload() {
     return (
-        <div>
-            FileUpload
+        <div style={{ display:'flex', justifyContent:'space-between' }}>
+            <Dropzone onDrop multiple maxSize>
+                {({getRootProps, getInputProps}) => (
+                    <div style={{width:'300px', height:'240px', border:'1px solid lightgray', display:'inline-flex', alignItems:'center', justifyContent:'center'}}
+                        {...getRootProps()}
+                    >
+                        <input {...getInputProps()} />
+                        <Icon type="plus" style={{fontSize: '3rem'}} />
+                        <label>Upload Images Here...</label>
+                    </div>
+                )}
+            </Dropzone>
         </div>
     )
 }
