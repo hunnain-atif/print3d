471bb229457a92eacecd6875f94b12d7d84fc33b
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-10-13 22:37:07 -0400

    finished onChange functions for all components on UploadPage

--- client/src/components/views/UploadPage/UploadPage.js
+++ client/src/components/views/UploadPage/UploadPage.js
@@ -1,13 +1,34 @@
 import React, {useState} from 'react'
+import { Typography, Button, Form, message, Input, Icon } from 'antd'; 
+const Categories = [
+    {key:1, value: "3D-Model"}, 
+    {key:2, value: "3D-Printed Object"}
+]
 
 function UploadPage() {
     
     const [TitleValue, setTitleValue] = useState("")
+    const [DescriptionValue, setDescriptionValue] = useState("")
+    const[PriceValue, setPriceValue] = useState(0)
+    const[CategoryValue, setCategoryValue] = useState(1)
 
     const onTitleChange = (event) => {
         setTitleValue(event.currentTarget.value)
 
     }
+
+    const onDescriptionChange = (event) => {
+        setDescriptionValue(event.currentTarget.value)
+    }
+
+    const onPriceChange = (event) => {
+        setPriceValue(event.currentTarget.value)
+    }
+
+    const onCategoryChange = (event) => {
+        setCategoryValue(event.currentTarget.value)
+    }
+
     return (
         <div style={{maxWidth: '700px', margin: '2rem auto'}}>
             <div style={{textAlign: 'center', marginBottom:'2rem'}}>
@@ -23,12 +44,18 @@ function UploadPage() {
                 <br />
                 <br />
                 <label>Description</label>
-                <textarea onChange value />
-                <label>Description</label>
+                <textarea onChange={onDescriptionChange} value={DescriptionValue} />
                 <br />
                 <br />
                 <label>Price ($)</label>
-                <input onChange value  type="number"/>
+                <input onChange={onPriceChange} value={PriceValue}  type="number"/>
+                <br />
+                <br />
+                <select onChange={onCategoryChange} value={CategoryValue}>
+                    {Categories.map(item => (
+                        <option key={item.key} value={item.key}>{item.value}</option>
+                    ))}
+                </select>
                 <br />
                 <br />
                 <button onClick>
