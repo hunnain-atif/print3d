631f073fb83fe37d6ecdb21c6e6f7a97a571d085
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-10-17 18:05:27 -0400

    uploaded products appear on landing page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,5 +1,9 @@
 import Axios from 'axios';
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useState} from 'react'; 
+import {Icon, Col, Row, Card} from 'antd'; 
+
+const { Meta } = Card; 
+// TODO: change slogan
 
 function LandingPage() {
     const [Products, setProducts] = useState([])
@@ -15,8 +19,39 @@ function LandingPage() {
             }
         })
     }, [])
+
+    const renderCards = Products.map((product, index)=> {
+        return <Col lg={6} md={8} xs={24}>
+            <Card hoverable={true} cover>
+                <Meta title={product.title} description={`$${product.price}`}/>
+            </Card>
+        </Col>
+    })
+
+
     return (
-        <div>
+        <div style={{width: '75%', margin: '3rem auto'}}>
+            <div style={{textAlign: 'center'}}>
+                <h2>print3d: Making 3D Printing Accessible <Icon type="printer"/></h2>
+            </div>
+
+            {/* Filter */}
+            {/* Search */}
+
+            {Products.length === 0? 
+                <div style={{display: 'flex', height: '300px', justifyContent: 'center', alignItems: "center"}}>
+                    <h2>No postings currently...</h2>
+                </div>  :
+                <div>
+                    <Row gutter={[16, 16]}>
+                        {renderCards}
+                    </Row>
+                </div>
+             }
+            <br /><br /> 
+            <div style={{ display:'flex', justifyContent:'center'}}>
+                 <button>Load More</button>
+            </div>
         </div>
     )
 }
