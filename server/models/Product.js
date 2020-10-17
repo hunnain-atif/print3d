52f1b3e1b098e1500714c4f6b8d54c354cb83b53
Hunnain Atif <h2atif@uwaterloo.ca>
2020-10-16 23:16:00 -0400

    create product model

---
+++ server/models/Product.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema
+
+const productSchema = mongoose.Schema({
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    title:  {
+        type: String,
+        maxlength: 50
+    },
+    description: {
+        type: String
+    },
+    price: {
+        type: Number,
+        default: 0
+    },
+    images: {
+        type: Array,
+        default: []
+    },
+    categories: {
+        type: Number,
+        default: 1
+    },
+    sold: {
+        type: Number,
+        maxlength: 100,
+        default: 0
+    },
+    views: {
+        type: Number,
+        default: 0
+    } 
+}, {timestamps: true})
+
+const User = mongoose.model('Product', productSchema);
+
+module.exports = { User }
