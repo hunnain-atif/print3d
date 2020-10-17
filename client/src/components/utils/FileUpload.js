87cc57d10c562d1a902906dfac48941140069d81
Hunnain Atif <h2atif@uwaterloo.ca>
2020-10-16 23:08:53 -0400

    add delete functionality for uploaded photos

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -26,6 +26,14 @@ function FileUpload(props) {
             })
     }
 
+    const onDelete = (image) => {
+        const currentIndex = Images.indexOf(image);
+        let newImages = [...Images];
+        newImages.splice(currentIndex, 1);
+        setImages(newImages)
+        props.refreshFunction(newImages)
+    }
+
     return (
         <div style={{ display:'flex', justifyContent:'space-between' }}>
             <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
@@ -42,7 +50,7 @@ function FileUpload(props) {
 
             <div style={{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll'}}>
                 {Images.map((image, index) => (
-                    <div onClick>
+                    <div onClick={() => onDelete(image)}>
                         <img style={{ minWidth:"300px", width:'300px', height:'240px'}} src={`http://localhost:5000/${image}`} alt={`productImage-${index}`}/>
                     </div>
                 ))}
