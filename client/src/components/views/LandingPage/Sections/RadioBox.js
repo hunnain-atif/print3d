b1e2d72d9008350c169283de0b0444bb2714bc47
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-10-18 12:13:02 -0400

    list data for radio box on landing page

---
+++ client/src/components/views/LandingPage/Sections/RadioBox.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const price = [
+    {
+        "_id": 0,
+        "name": "Any",
+        "array": []
+    },
+    {
+        "_id": 1,
+        "name": "$0 to $24.99",
+        "array": [0, 24.99]
+    },
+    {
+        "_id": 2,
+        "name": "$25 to $49.99",
+        "array": [25, 49.99]
+    },
+    {
+        "_id": 3,
+        "name": "$50 to $74.99",
+        "array": [50, 74.99]
+    },
+    {
+        "_id": 4,
+        "name": "$75 to $99.99",
+        "array": [75, 99.99]
+    },
+    {
+        "_id": 5,
+        "name": "More than $100",
+        "array": [100, 1500000]
+    }
+]
+
+function RadioBox() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RadioBox
