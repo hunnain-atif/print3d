92604d72084eace9462b233864f050bf03ed33c8
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-10-18 00:23:01 -0400

    categories list data for filter section on landing page

--- client/src/components/views/LandingPage/Sections/CheckBox.js
+++ client/src/components/views/LandingPage/Sections/CheckBox.js
@@ -1,5 +1,17 @@
 import React from 'react'
 
+
+const categories = [
+    {
+        "_id": 1, 
+        "name": "3D-Model"
+    }, 
+    {
+        "_id": 2, 
+        "name": "3D-Printed Object"
+    }
+]
+
 function CheckBox() {
     return (
         <div>
