2d743f4b4988b45ea46929a1ff561c1fc02e1cd4
Gobishan Vijikaran <gvijikar@edu.uwaterloo.ca>
2020-11-06 22:30:14 -0500

    cart page file creation + route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
 import UploadPage from './views/UploadPage/UploadPage';
 import DetailProductPage from './views/DetailProductPage/DetailProductPage';
+import CartPage from './views/CartPage/CartPage'; 
 
 //null   Anyone Can go inside
 //true   only logged in user can go inside
@@ -25,6 +26,7 @@ function App() {
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
           <Route exact path="/product/upload" component={Auth(UploadPage, true)} />
           <Route exact path="/product/:productId" component={Auth(DetailProductPage, null)} />
+          <Route exact path="/user/cart" component={Auth(CartPage, true)} />
         </Switch>
       </div>
       <Footer />
